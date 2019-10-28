290f6ea204fd235f6e97822c8a868898e47a474f
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2019-10-28 14:51:41 -0300

    02.01_Instalando_JWT

--- api/__init__.py
+++ api/__init__.py
@@ -3,12 +3,14 @@ from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_marshmallow import Marshmallow
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 migrate = Migrate(app, db)
+jwt = JWTManager(app)
 
 api = Api(app)
 
